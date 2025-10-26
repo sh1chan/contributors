50750457ecea61e5cc6a82b73918d7d0c4b3e554
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-10-27 01:30:51 +0300

    [auth] Updated routes `register_post`
    - Added `OAuth2*` example

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,13 +1,21 @@
 """ Auth Router
 """
 
+from typing import Annotated
+
 from fastapi import APIRouter
 from fastapi import Request
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
 
 from src.core.app import template_files
+from src.core.db import MongoDB
+from src.core.db import DBCollectionsEnum
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @auth_router.get("/register")
@@ -19,5 +27,16 @@ async def register_get(request: Request):
 
 
 @auth_router.post("/register")
-async def register_post():
-    return {}
+async def register_post(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+):
+    """
+    """
+    collection = await MongoDB.collection(DBCollectionsEnum.users)
+    result = await collection.insert_one(
+        {
+            "username": form_data.username,
+            "password": form_data.password,
+        }
+    )
+    return str(result.inserted_id)
