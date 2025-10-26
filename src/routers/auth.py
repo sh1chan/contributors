33aa6bcf143d290b453363c004875f0fec9d9ae3
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-10-26 23:37:06 +0300

    [auth] Added template files `register.html`
    Updated router `register_get` to response a template file

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,12 +1,21 @@
+""" Auth Router
+"""
+
 from fastapi import APIRouter
+from fastapi import Request
+
+from src.app import template_files
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_router.get("/register")
-async def register_get():
-    return {}
+async def register_get(request: Request):
+    return template_files.TemplateResponse(
+        request=request,
+        name="auth/register.html",
+    )
 
 
 @auth_router.post("/register")
