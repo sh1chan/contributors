c62dcbef2d12648f8733d26a14fea25f2cbdbb42
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-10-31 03:32:34 +0300

    [issues] Added routers `issues_get`
    - Added template files `issues.html`

--- src/routers/issues.py
+++ src/routers/issues.py
@@ -1,9 +1,24 @@
 from fastapi import APIRouter
+from fastapi import Request
+from fastapi import Depends
+
+from src.core.app import template_files
+from src.routers.auth import get_optional_current_user
 
 
 issues_router = APIRouter(prefix="/issues", tags=["issues"])
 
 
-@issues_router.get("/")
-async def root_get():
-    return {}
+@issues_router.get("/", name="issues_get")
+async def issues_get(
+    request: Request,
+    current_user=Depends(get_optional_current_user),
+):
+    return template_files.TemplateResponse(
+        request=request,
+        name="issues/issues.html",
+        context={
+            "user": current_user,
+            "issues": [],
+        }
+    )
