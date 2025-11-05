69d219c857adb73498a798160a09d4303d30b605
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-11-05 12:56:07 +0300

    [issues] Added schemas `IssuesNewIn`
    - Updated schemas `IssuesModel`

--- src/schemas/issues.py
+++ src/schemas/issues.py
@@ -4,18 +4,54 @@ from pydantic import ConfigDict
 from pydantic import Field
 from pydantic import AnyHttpUrl
 from pydantic import AwareDatetime
+from pydantic import model_validator
+from pydantic import computed_field
+from typing_extensions import Self
 
 
 class IssuesModel(BaseModel):
     id: ObjectId = Field(..., alias="_id")
-    url: AnyHttpUrl | None
+    url: AnyHttpUrl | str = Field("")
     title: str
     description: str | None = Field(None)
-    tags: list[ObjectId] = Field([])
-    labels: list[ObjectId] = Field([])
+    tags: list[str] = Field([])
+    labels: list[str] = Field([])
     projects: list[ObjectId] = Field([])
     added_by: ObjectId | None = Field(None)
     created_by: ObjectId | None = Field(None)
     creation_dt: AwareDatetime
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class IssuesNewIn(BaseModel):
+    url: str
+    title: str
+    description: str
+    tags: str
+    labels: str
+
+    @staticmethod
+    def seperate_field_values(field: str, delimiter: str = ',') -> list[str]:
+        if not field:
+            return []
+        return [v.strip() for v in field.split(delimiter)]
+
+    @model_validator(mode='after')
+    def clean_up_fields(self) -> Self:
+        self.url = self.url.strip()
+        self.title = self.title.strip()
+        self.description = self.description.strip()
+        self.tags = self.tags.strip()
+        self.labels = self.labels.strip()
+        return self
+
+    @computed_field
+    @property
+    def all_tags(self) -> list[str]:
+        return self.seperate_field_values(self.tags)
+
+    @computed_field
+    @property
+    def all_labels(self) -> list[str]:
+        return self.seperate_field_values(self.labels)
