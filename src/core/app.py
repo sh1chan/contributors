4d3e977b42b277c9ce05e59ae867ebbc6099e3a8
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-11-17 03:08:19 +0300

    [cure] Added application params `exception_handlers`
    - Added `exception_handler_ice`, redirects to routers `get_login`

--- src/core/app.py
+++ src/core/app.py
@@ -1,10 +1,15 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi import Response
+from fastapi import status
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 from src.core.db import MongoDB
+from src.exceptions.auth import InvalidCredentialsException
 
 
 static_files = StaticFiles(directory="src/static")
@@ -17,6 +22,20 @@ async def lifespan(app: FastAPI):
     yield
 
 
+async def exception_handler_ice(
+    request: Request, exc: InvalidCredentialsException,
+) -> Response:
+    redirect_url = request.url_for(
+        "get_login"
+    ).include_query_params(
+        error_message="Could not validate credentials.",
+    )
+    return RedirectResponse(
+        url=redirect_url,
+        status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+    )
+
+
 def get_app() -> FastAPI:
     from src.routers.auth import auth_router
     from src.routers.projects import projects_router
@@ -24,6 +43,9 @@ def get_app() -> FastAPI:
 
     app = FastAPI(
         lifespan=lifespan,
+        exception_handlers={
+            InvalidCredentialsException: exception_handler_ice,
+        }
     )
 
     app.include_router(auth_router)
